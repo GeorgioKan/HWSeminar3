2b991393e1c5a460d042b2c285a0347895b1061b
Gerorge <logovas4@gmail.com>
2023-01-21 16:43:12 +0300

    Task2 with FOR and SWITCH update

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,23 +6,42 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 int[] coordsA = new int[3];
-System.Console.WriteLine("Ax:");
-coordsA[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Ay:");
-coordsA[1] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Az:");
-coordsA[2] = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < coordsA.Length; i++)
+{
+    switch (i)
+    {
+        case 0:
+            System.Console.WriteLine("Ax:");
+            break;
+        case 1:
+            System.Console.WriteLine("Ay:");
+            break;
+        case 2:
+            System.Console.WriteLine("Az:");
+            break;
+    }
+    coordsA[i] = Convert.ToInt32(Console.ReadLine());
+}
 
 int[] coordsB = new int[3];
-System.Console.WriteLine("Bx:");
-coordsB[0] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("By:");
-coordsB[1] = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine("Bz:");
-coordsB[2] = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < coordsA.Length; i++)
+{
+    switch (i)
+    {
+        case 0:
+            System.Console.WriteLine("Bx:");
+            break;
+        case 1:
+            System.Console.WriteLine("By:");
+            break;
+        case 2:
+            System.Console.WriteLine("Bz:");
+            break;
+    }
+    coordsB[i] = Convert.ToInt32(Console.ReadLine());
+}
 
 double d = Math.Sqrt(Math.Pow(coordsB[0] - coordsA[0], 2) +
                      Math.Pow(coordsB[1] - coordsA[1], 2) +
                      Math.Pow(coordsB[2] - coordsA[2], 2));
 System.Console.WriteLine(Math.Round(d, 2));
-
