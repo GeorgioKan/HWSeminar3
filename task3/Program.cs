54f1c9fa82be271eec64813fa64f674f7d8ea3ac
Gerorge <logovas4@gmail.com>
2023-01-21 16:58:41 +0300

    Task3 added

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+System.Console.WriteLine("Введите число N: ");
+double N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+    for (int i = 0; i <= N; i++)
+    {
+        double answer = Math.Pow(i,3);
+        System.Console.WriteLine(answer + " ");
+    }
+}
+else
+System.Console.WriteLine("Число отрицательно или равно 0");
